cfd83b6e02a696d015ce92299f6ca1529ce59885
Ivan Bryakunov <ivan.bryakunov@gmail.com>
2022-03-26 20:23:22 +0400

    Почти CRUD над base

--- src/app.js
+++ src/app.js
@@ -1,10 +1,49 @@
 
-const express = require('express')
+const express = require('express');
+const crypto = require("crypto");
+
+const id = crypto.randomBytes(16).toString("hex");
 const app = express();
-const port = 3000
+const port = 3000;
+
+const urlencodedParser = express.urlencoded({extended: false});
+
+let base = [];
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/body', urlencodedParser,  (req, res) => {
+  res.send(req.body)
+})
+
+app.post('/base', urlencodedParser,  (req, res) => {
+  const newEntity = {...req.body, _id: crypto.randomBytes(16).toString("hex")};
+
+  base.push(newEntity);
+
+  res.send(newEntity);
+})
+
+app.get('/base', (req, res) => {
+  res.send(base)
+})
+
+app.delete('/base', urlencodedParser, (req, res) => {
+  let resData = null;
+
+  console.log(req.body)
+  base = base.filter(item => {
+    if(item._id === req.body.id) {
+      resData = item;
+      return false;
+    }
+
+    return true;
+  });
+
+  res.send(resData)
 })
 
 app.listen(port, () => {
